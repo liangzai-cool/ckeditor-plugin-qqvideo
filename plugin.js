1cb92ac3d06dea3d28fb94854d35e50840381ca9
liangzai_cool <liangzai_cool@yeah.net>
2016-10-09 11:15:14 +0800

    fix a bug

--- plugin.js
+++ plugin.js
@@ -7,13 +7,14 @@
     beforeInit: function(editor){
     },
     init: function(editor) {
-            editor.addCommand(pluginName, new CKEDITOR.dialogCommand(pluginName));
-            editor.ui.addButton(pluginName, {
-              label: lang.button.label,
-              command: pluginName,
-              icon: this.path + 'images/icon.png'
-            });
-            CKEDITOR.dialog.add(pluginName, this.path + 'dialogs/qqvideo.js');
+      var lang = editor.lang[pluginName];
+      editor.addCommand(pluginName, new CKEDITOR.dialogCommand(pluginName));
+      editor.ui.addButton(pluginName, {
+        label: lang.button.label,
+        command: pluginName,
+        icon: this.path + 'images/icon.png'
+      });
+      CKEDITOR.dialog.add(pluginName, this.path + 'dialogs/qqvideo.js');
     },
     afterInit: function(editor){
       
